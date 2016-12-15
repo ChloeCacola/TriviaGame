94fc7721e98e7aa996d1a45fa9b8d6f7c4ac9717
ChloeCacola <cscorrigan@icloud.com>
2016-12-15 15:18:16 -0500

    Unstyled working game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,8 @@ var counter = setInterval(timer, 1000);
 var timeUp = setTimeout(timeUp, 1000 * 10); //60 seconds
 var correct = 0
 var incorrect = 0
-var unanswered = 6
-var answers = []
+var unanswered = 0
+var answers = ["filler"] //filler used so IF/ELSE statement will run and count unanswered questions appropriately 
 var numQues = 6 //change this if/when more/less questions added
 
 
@@ -33,6 +33,9 @@ $('#Q6 input').on('change', function() {
    answers.push($('input[name=A6]:checked', '#Q6').val()); 
 });
 
+//Hide results
+$("#results").css('display', 'none');
+
 
 //TIMER
 function timer() {
@@ -43,7 +46,7 @@ function timer() {
 		return;
 	}
 	// document.getElementById("currentTimeLeft").innerHTML=count + " seconds";
-	$("#currentTimeLeft").html(count + " seconds");
+	$("#time").html(count + " seconds");
 }
 
 //Time is up:  results are revealed on a new page.
@@ -61,13 +64,22 @@ function timeUp() {
 
 	if (answers.length < numQues) {
 		unanswered = numQues - (incorrect + correct)
-	} 
+	} else {
+		unanswered = numQues
+	}
 };
 
-//hidden forms
+//hide questions/answers
+$("#game").css('display', 'none');
 
 
 //printing results on page
+$("#results").css('display', 'initial');
+
+
+$("#correct").html("Correct: " + correct);
+$("#incorrect").html("Incorrect: " + incorrect);
+$("#unanswered").html("Unanswered: " + unanswered);
 console.log(correct);
 console.log(incorrect);
 console.log(unanswered);
